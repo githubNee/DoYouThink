d912a8b49b7b7938296f87a691f24559d419b987
Nee <nee_11235@qq.com>
2017-07-01 16:37:41 +0800

    data load

--- routes/pan.js
+++ routes/pan.js
@@ -8,8 +8,21 @@ router.route('/public')
 	var url = 'http://rsf.qbox.me/list?bucket=' + config.bucket.public;
 	util.qiniu_request(url, "POST", function(body) {
 		var ret = JSON.parse(body);
-		for (var index = 0; index < ret['items'].length; index++ )
+		for (var index = 0; index < ret['items'].length; index++ ) {
 			ret['items'][index]['link'] = config.domain.public + '/' + ret['items'][index]['key'];
+
+			var fsize = ret['items'][index]['fsize'];
+			if (fsize < 1024)
+				ret['items'][index]['fsize'] = fsize.toFixed(2).toString() + 'B';
+			else if (fsize < 1024 * 1024)
+				ret['items'][index]['fsize'] = (fsize / 1024).toFixed(2).toString() + 'KB';
+			else
+				ret['items'][index]['fsize'] = (fsize / 1024 / 1024).toFixed(2).toString() + 'MB';
+
+			var nDate = new Date();
+			nDate.setTime(ret['items'][index]['putTime'] / 10000);
+			ret['items'][index]['putTime'] = nDate.toLocaleDateString().split('T')[0];
+		}
 		res.send(ret);
 	});
 });
