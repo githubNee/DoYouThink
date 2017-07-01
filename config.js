f839d409570f84f3be129515bac880b6d0447d4b
Nee <nee_11235@qq.com>
2017-07-01 14:15:32 +0800

    config

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+	"bucket": {
+		"public": "public",
+		"private": "private"
+	},
+	"mac": {
+		"accessKey": "",
+		"secretKey": ""
+	},
+	"domain": {
+		"public": "",
+		"private": ""
+	},
+	"admin": {
+		"name": "",
+		"password": ""
+	}
+};
+
+module.exports = config
