ba44896debeaea6504a8ab8b32e823d0b6f103bd
Nee <nee_11235@qq.com>
2017-07-06 23:19:31 +0800

    remove cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var server = http.createServer(app);
 
 var path = require('path');
 var port = process.env.PORT || 3000;
-server.listen(port, '0.0.0.0');
+server.listen(port);
 var request = require('request');
 var bodyParser = require('body-parser');
 
@@ -14,12 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , PRIVATE-TOKEN');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
 
 var pan = require('./routes/pan');
 app.use('/api/pan', pan);
